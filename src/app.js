23651162cd3a03d84cdf20cd8ab7d9d7e54a0510
Michael Primavera <pastasauce59@gmail.com>
2022-01-15 14:24:49 -0500

    added event listener to aboutMeButton div instead of <a> tag for better funtionality

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 let hamburgerButton = document.getElementById('hamburger')
 let navList =  document.getElementById('nav-list')
+let aboutMeButton = document.getElementById('aboutMeButton')
 
 let showNavList = () => {
     navList.classList.toggle('show')
 }
 
 hamburgerButton.addEventListener('click', showNavList)
+
+aboutMeButton.addEventListener('click', () => {
+    window.location = '#about-me'
+})
